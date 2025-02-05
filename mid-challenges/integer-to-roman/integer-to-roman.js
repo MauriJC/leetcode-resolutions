aa9ee4e6764e28ce9fd27cb7d0ac46210772acac
MauriJC <xaviermauricio33@gmail.com>
2025-02-05 18:20:44 -0300

    added integer to roman

---
+++ mid-challenges/integer-to-roman/integer-to-roman.js
@@ -0,0 +1,20 @@
+"use strict";
+console.log("integer-to-roman loaded");
+function intToRoman(num) {
+    // Mapeo de valores en orden descendente
+    const romanMap = [
+        [1000, "M"], [900, "CM"], [500, "D"], [400, "CD"],
+        [100, "C"], [90, "XC"], [50, "L"], [40, "XL"],
+        [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]
+    ];
+    let result = "";
+    for (const [value, symbol] of romanMap) {
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
+    }
+    return result;
+}
+;
+intToRoman(31239);
